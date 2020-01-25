69fe495236ec7d8f767d44e7cb0ae361688fd484
Евгений Зайцев <zayg21@hotmail.com>
2020-01-26 00:31:43 +0300

    Migration to NetStandard

--- Sources/RandomAlgebra/Distributions/SpecialDistributions/ArcsineDisctribution.cs
+++ Sources/RandomAlgebra/Distributions/SpecialDistributions/ArcsineDisctribution.cs
@@ -1,10 +1,6 @@
 ﻿using Accord;
 using Accord.Statistics.Distributions.Univariate;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RandomAlgebra.Distributions
 {
