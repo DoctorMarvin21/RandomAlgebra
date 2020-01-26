e7a16487d6849e2b777aaf1f546d0a13b517cc52
Евгений Зайцев <zayg21@hotmail.com>
2020-01-26 19:07:21 +0300

    Added unit tests Fixed us resorces fails

---
+++ Sources/RandomAlgebra.Test/ResourcesTests.cs
@@ -0,0 +1,94 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+
+namespace RandomAlgebra.Test
+{
+    [TestClass]
+    public class UsResourcesTests
+    {
+        [TestInitialize]
+        public void SetLocale()
+        {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-US");
+        }
+
+
+        [TestMethod]
+        public void TestArgumentExceptions()
+        {
+            var values = Enum.GetValues(typeof(DistributionsArgumentExceptionType))
+                .Cast<DistributionsArgumentExceptionType>().ToArray();
+
+            Assert.IsTrue(values.Length > 0);
+
+            foreach (DistributionsArgumentExceptionType value in values)
+            {
+                string message = ExceptionMessages.GetExceptionMessage(value.ToString());
+                Assert.IsNotNull(message);
+            }
+        }
+
+        [TestMethod]
+        public void TestInvalidOperationExceptions()
+        {
+            var values = Enum.GetValues(typeof(DistributionsInvalidOperationExceptionType))
+                .Cast<DistributionsInvalidOperationExceptionType>().ToArray();
+
+            Assert.IsTrue(values.Length > 0);
+
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+
+            foreach (DistributionsInvalidOperationExceptionType value in values)
+            {
+                string message = ExceptionMessages.GetExceptionMessage(value.ToString());
+                Assert.IsNotNull(message);
+            }
+        }
+    }
+
+        [TestClass]
+    public class RuResourcesTests
+    {
+        [TestInitialize]
+        public void SetLocale()
+        {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("ru-RU");
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("ru-RU");
+        }
+
+
+        [TestMethod]
+        public void TestArgumentExceptions()
+        {
+            var values = Enum.GetValues(typeof(DistributionsArgumentExceptionType))
+                .Cast<DistributionsArgumentExceptionType>().ToArray();
+
+            Assert.IsTrue(values.Length > 0);
+
+            foreach (DistributionsArgumentExceptionType value in values)
+            {
+                string message = ExceptionMessages.GetExceptionMessage(value.ToString());
+                Assert.IsNotNull(message);
+            }
+        }
+
+        [TestMethod]
+        public void TestInvalidOperationExceptions()
+        {
+            var values = Enum.GetValues(typeof(DistributionsInvalidOperationExceptionType))
+                .Cast<DistributionsInvalidOperationExceptionType>().ToArray();
+
+            Assert.IsTrue(values.Length > 0);
+
+            foreach (DistributionsInvalidOperationExceptionType value in values)
+            {
+                string message = ExceptionMessages.GetExceptionMessage(value.ToString());
+                Assert.IsNotNull(message);
+            }
+        }
+    }
+}
